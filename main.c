61d27c7166bc1ba07e26dcc4e6d4240d3ce07929
Daniel Truong <aduy1122@gmail.com>
2022-11-25 11:08:53 +0000

    ✨ Adding timer + timeout logic + refactor some codes

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // constants
 #define MAX_THREADS 10;
@@ -21,6 +22,9 @@ int max_rand = MAX_RAND;
 int num_in_queue = NUM_IN_QUEUE;
 int max_steps = MAX_STEPS;
 int process_dur = PROCESS_DUR;
+time_t start_time = 0;
+time_t current_time = 0;
+double elapsed_time = 0;
 
 typedef struct {
   int index;
@@ -71,7 +75,7 @@ int main(int argc, char *argv[]) {
       ++next_idx;
     } else {
       // unknown arg
-      printf("invalid command %s\n", argv[next_idx]);
+      printf("ERROR! invalid command %s\n", argv[next_idx]);
       help_menu();
       return 0;
     }
@@ -96,20 +100,24 @@ int main(int argc, char *argv[]) {
   mqd_t message_queue = mq_open(MESSAGE_QUEUE_NAME, O_RDWR | O_CREAT | O_EXCL,
                                 0644, &queue_attrib);
   if (message_queue == (mqd_t)-1) {
-    fprintf(stderr, "Can't open message queue \n \n");
+    fprintf(stderr, "ERROR! Can't open message queue \n \n");
     perror("mq_open");
     exit(0);
   } else {
-    if (debug) {
-      printf("Successfully open msg queue \n \n");
-    }
+    printf("Successfully open msg queue \n \n");
+  }
+
+  // Start timer
+  time(&start_time);
+  if (debug) {
+    printf("--- TIME STARTING ---: %ld \n \n", start_time);
   }
   // PRODUCER
   pthread_t producer;
   int producer_status =
       pthread_create(&producer, NULL, (void *)fill_msg_queue, &message_queue);
   if (producer_status != 0) {
-    printf("Error creating producer thread \n \n");
+    printf("ERROR! Can't create producer thread \n \n");
     exit(1);
   } else {
     if (debug) {
@@ -124,7 +132,7 @@ int main(int argc, char *argv[]) {
     int consumer_status =
         pthread_create(&consumer[i], NULL, (void *)process_msg, &message_queue);
     if (consumer_status != 0) {
-      printf("Error creating consumer thread \n \n");
+      printf("ERROR! Can't create consumer thread \n \n");
       exit(1);
     } else {
       if (debug) {
@@ -135,17 +143,18 @@ int main(int argc, char *argv[]) {
   // Wait for producer thread to be done
   int producer_join_status = pthread_join(producer, NULL);
   if (producer_join_status != 0) {
-    printf("Error joining producer thread \n \n");
+    printf("ERROR! can't join producer thread \n \n");
     exit(1);
   } else {
     if (debug) {
       printf("Producer thread is joined! \n \n");
     }
   }
-  // while queue not empty
-  // sleep (1)
+
   struct mq_attr empty_queue_test;
   mq_getattr(message_queue, &empty_queue_test);
+  // while queue not empty
+  // sleep (1)
   while (empty_queue_test.mq_curmsgs) {
     sleep(1);
     mq_getattr(message_queue, &empty_queue_test);
@@ -154,7 +163,7 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < max_threads; i++) {
     int cancel_status = pthread_cancel(consumer[i]);
     if (cancel_status != 0) {
-      printf("Error canceling thread \n \n");
+      printf("ERROR! Can't cancel thread \n \n");
       exit(1);
     } else {
       if (debug) {
@@ -166,7 +175,7 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < max_threads; i++) {
     int consumer_join_status = pthread_join(consumer[i], NULL);
     if (consumer_join_status != 0) {
-      printf("Error joining consumer thread \n \n");
+      printf("ERROR! Can't join consumer thread \n \n");
       exit(1);
     } else {
       if (debug) {
@@ -175,6 +184,11 @@ int main(int argc, char *argv[]) {
     }
   }
   int close_stat = mq_close(message_queue);
+  if (close_stat != 0) {
+    printf("ERROR! Can't close message queue \n \n");
+  } else {
+    printf("Message queue closed! \n \n");
+  }
   mq_unlink(MESSAGE_QUEUE_NAME);
   return 0;
 }
@@ -190,6 +204,17 @@ void fill_msg_queue(mqd_t *msq) {
     buffer->original_number = (rand() % (upper - lower + 1)) + lower;
     buffer->calculated_number = buffer->original_number;
     buffer->number_of_steps = 0;
+    // Calculate time
+    time(&current_time);
+    elapsed_time = difftime(current_time, start_time);
+    if (debug) {
+      printf("--- Elapsed Time is: %lf \n \n", elapsed_time);
+    }
+    if (elapsed_time > process_dur) {
+      printf("Timeout limit exceeded, number of steps: %d \n \n",
+             buffer->number_of_steps);
+      exit(1);
+    }
     printf("Putting message (id: %d, value: %d ) to message queue \n \n",
            buffer->index, buffer->original_number);
     if (debug) {
@@ -203,14 +228,16 @@ void fill_msg_queue(mqd_t *msq) {
         mq_send(*msq, (void *)buffer, sizeof(MessageUnit), MESSAGE_PRIORITY);
     sleep(1);
     if (status == -1) {
-      printf("Error putting number to msg queue \n \n");
+      printf("ERROR! Can't put number to msg queue \n \n");
     } else {
       if (debug) {
         printf("Successfully putting number to msg queue \n \n");
       }
     }
   }
-  printf("Done putting number in msg queue \n \n");
+  if (debug) {
+    printf("Done putting number in msg queue \n \n");
+  }
 }
 
 void process_a_msg(mqd_t *msq) {
@@ -218,10 +245,20 @@ void process_a_msg(mqd_t *msq) {
   struct mq_attr attr;
   mq_getattr(*msq, &attr);
   MessageUnit *buffer = malloc(sizeof(MessageUnit));
-  // pop the front of the msg queue into var (mq_receive)
-  unsigned nr = mq_receive(*msq, (void *)buffer, attr.mq_msgsize, NULL);
+  // Calculate time
+  time(&current_time);
+  elapsed_time = difftime(current_time, start_time);
   if (debug) {
-    printf("Read %u bytes from MQ\n", nr);
+    printf("--- Elapsed Time is: %lf \n \n", elapsed_time);
+  }
+  if (elapsed_time > process_dur) {
+    printf("Timeout limit exceeded, number of steps: %d \n \n",
+           buffer->number_of_steps);
+    exit(1);
+  }
+  // pop the front of the msg queue into var (mq_receive)
+  mq_receive(*msq, (void *)buffer, attr.mq_msgsize, NULL);
+  if (debug) {
     printf("Original number was %d\n", buffer->original_number);
     printf("Calculated number was %d\n", buffer->calculated_number);
   }
